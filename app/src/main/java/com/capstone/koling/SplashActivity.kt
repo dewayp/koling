044bf7164d03c1031e2c5d52ecfab5a30a4d2c29
dewayp <dewayoganteng1@gmail.com>
2023-05-27 00:11:12 +0800

    slicing-awal

---
+++ app/src/main/java/com/capstone/koling/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.capstone.koling
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DELAY: Long = 3000 // 3 detik
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Membuat tunda selama beberapa detik sebelum berpindah ke activity berikutnya
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DELAY)
+    }
+}
+
